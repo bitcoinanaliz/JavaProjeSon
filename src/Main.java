9971d2a139c7956456005742f8630b8afe0af605
Bitcoin Analiz <bitcoinanaliz@gmail.com>
2018-10-30 14:36:12 +0300

    Sadeleştirme
    Sadeleştirme

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,14 @@ public class Main {
 
     public static void main(String[] args){
 
-
-        System.out.println("Test");
-
-
-
-        System.out.println("Git Hub İkinci Paylaşım");
-
-        System.out.println("Bu ikinci ders");
-
-       
-
-        System.out.println("Deneme ve Öğrenme");
-
-        System.out.println("Süper GitHub Desktop Çalıştı");
+        System.out.println("Git Hub Çalışıyor");
         
-         int yil = 2018;
+        int yil = 2018;
         int gelecek = 2035;
         int kalan;
         kalan = gelecek - yil;
 
-        System.out.println("Kalan Yıl " + kalan + " 100");
+        System.out.println("Kalan Yıl " + kalan );
 
 
     }
