7baeeb7d2cdbd573c313e17ab894b3df49a804eb
bitcoinanaliz <44569566+bitcoinanaliz@users.noreply.github.com>
2018-10-30 14:34:13 +0300

    github yıl değiştirme
    sitede değiştirme yapıldı

--- src/Main.java
+++ src/Main.java
@@ -12,16 +12,18 @@ public class Main {
 
         System.out.println("Bu ikinci ders");
 
-        int yil = 2018;
+       
+
+        System.out.println("Deneme ve Öğrenme");
+
+        System.out.println("Süper GitHub Desktop Çalıştı");
+        
+         int yil = 2018;
         int gelecek = 2035;
         int kalan;
         kalan = gelecek - yil;
 
         System.out.println("Kalan Yıl " + kalan + " 100");
-
-        System.out.println("Deneme ve Öğrenme");
-
-        System.out.println("Süper GitHub Desktop Çalıştı");
 
 
     }
